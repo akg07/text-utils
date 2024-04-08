d583789233e187ac0aadeeda24f000a97107bb21
Ayush <ayush3032@gmail.com>
2024-04-09 00:50:50 +0530

    remove routers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 // Props are read-only 
 // do not change it here
 // you can only use the value which is provided
@@ -9,7 +9,7 @@ export default function Navbar(props) {
     <>
         <nav className={`navbar navbar-expand-lg bg-body-tertiary bg-${props.mode}`} data-bs-theme={props.mode}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">{props.title}</Link>
+                <a className="navbar-brand" href="/">{props.title}</a>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -23,11 +23,11 @@ export default function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
-                        </li>
+                        {/* <li className="nav-item">
+                            <a className="nav-link" href="/about">About</a>
+                        </li> */}
                     </ul>
                     <div className={ `form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}` }>
                         <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
